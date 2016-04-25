065de267d13b741678e5f0c8f1e3dbea4175a54b
Alex Macleod <alex@macleod.io>
2016-04-26 00:26:02 +0100

    More fields (NSFW, etc)

--- app/src/main/java/me/ccrama/redditslide/Databases.java
+++ app/src/main/java/me/ccrama/redditslide/Databases.java
@@ -30,12 +30,18 @@ public class Databases {
         public static final String COLUMN_HIDDEN = "hidden";
         public static final String COLUMN_DETAIL_ID = "detail_id";
         public static final String COLUMN_SORT_ORDER = "sort_order";
+        public static final String COLUMN_THEME_PRIMARY = "theme_primary";
+        public static final String COLUMN_THEME_ACCENT = "theme_accent";
+        public static final String COLUMN_PREVIEW_BIG = "preview_big";
+        public static final String COLUMN_PREVIEW_SELF = "preview_self";
     }
 
     public static abstract class SubredditDetail implements BaseColumns {
         public static final String TABLE_NAME = "subreddit_detail";
         public static final String COLUMN_SUBSCRIBED = "subscribed";
         public static final String COLUMN_MODERATOR = "moderator";
+        public static final String COLUMN_NSFW = "nsfw";
+        public static final String COLUMN_MOBILE_COLOR = "mobile_color";
     }
 
     public static abstract class MultiredditDetail implements BaseColumns {
@@ -61,17 +67,33 @@ public class Databases {
                     RedditEntry.COLUMN_TYPE + " TEXT," +
                     RedditEntry.COLUMN_HIDDEN + " BOOLEAN," +
                     RedditEntry.COLUMN_DETAIL_ID + " INTEGER," +
-                    RedditEntry.COLUMN_SORT_ORDER + " INTEGER" + ")");
+                    RedditEntry.COLUMN_SORT_ORDER + " INTEGER" +
+                    RedditEntry.COLUMN_THEME_PRIMARY + " TEXT," +
+                    RedditEntry.COLUMN_THEME_ACCENT + " TEXT," +
+                    RedditEntry.COLUMN_PREVIEW_BIG + " BOOLEAN," +
+                    RedditEntry.COLUMN_PREVIEW_SELF + " BOOLEAN," +
+                    ")");
             db.execSQL("CREATE TABLE " + SubredditDetail.TABLE_NAME + "(" +
                     SubredditDetail._ID + " INTEGER PRIMARY KEY," +
                     SubredditDetail.COLUMN_SUBSCRIBED + " BOOLEAN," +
-                    SubredditDetail.COLUMN_MODERATOR + " BOOLEAN" + ")");
+                    SubredditDetail.COLUMN_MODERATOR + " BOOLEAN," +
+                    SubredditDetail.COLUMN_NSFW + " BOOLEAN," +
+                    SubredditDetail.COLUMN_MOBILE_COLOR + " TEXT," +
+                    ")");
             db.execSQL("CREATE TABLE " + MultiredditDetail.TABLE_NAME + "(" +
                     MultiredditDetail._ID + " INTEGER PRIMARY KEY," +
                     MultiredditDetail.COLUMN_PATH + " TEXT," +
-                    MultiredditDetail.COLUMN_OWNER + " TEXT COLLATE NOCASE," + ")");
-
+                    MultiredditDetail.COLUMN_OWNER + " TEXT COLLATE NOCASE," +
+                    ")");
+            // TODO: Indexes, drop trailing commas
         }
+
+        /* NOTES:
+
+        entries recalled default all unhidden sorted in user order, optional bit flags to alter?
+        e.g. RedditStorage.getSubreddit(), ...(RedditStorage.NO_NSFW|RedditStorage.ALL)
+
+         */
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
